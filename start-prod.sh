c979588d5410e7c2a13cf57e4992bb4302a2b4df
Matiboux <matiboux@gmail.com>
2025-01-06 01:13:54 +0100

    Update start-prod.sh script

--- start-prod.sh
+++ start-prod.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+set -e
+
+# Check Docker Compose is available
+if ! command -v docker > /dev/null; then
+	echo ''
+	echo "The 'docker' command is not available! Please install Docker:"
+	echo "https://docs.docker.com/get-docker/"
+	echo ''
+	exit 1
+fi
 
 # Start in production
-dockerc prod up --build -d
+if command -v dockerc > /dev/null; then
+	# Use DockerC
+	dockerc prod up --build -d
+
+else
+	# Use Docker Compose
+	docker compose \
+		-f ./docker-compose.yml -f ./docker-compose.prod.yml \
+		--env-file ./.env --env-file ./.env.prod \
+		up --build -d
+fi
